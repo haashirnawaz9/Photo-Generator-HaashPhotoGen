a7eefb4c42c46460c3676341d07c9a910f17b1cf
Haashir Nawaz <153224457+haashirnawaz9@users.noreply.github.com>
2024-06-03 21:58:54 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,11 @@ const axios = require("axios");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['POST', 'GET', 'OPTIONS'],
+    credentials: true
+}));
 app.use(express.json());
 
 app.post("/chatbot", async (req, res) => {
